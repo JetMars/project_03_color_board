1923148ca9aa147c6dc2f7a28c4697e1d2eb191d
Marsel <imanager666@gmail.com>
2022-01-22 12:21:36 +0300

    add_clor_board

---
+++ app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const board = document.querySelector('#board');
+const SQUARES_NUMBER = 600;
+const colors = ['#07ede9', '#1d06b8', '#f71307', '#e67e22', '#f71307',
+'#c5d609', '#1491ba', '#38ba14']
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+
+  square.addEventListener('mouseover', () =>
+  setColor(square));
+
+  square.addEventListener('mouseleave', () =>
+  removeColor(square));
+
+  board.append(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor()
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = '#1d1d1d';
+  element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
